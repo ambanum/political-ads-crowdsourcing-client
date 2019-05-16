a6e5e896286fe242accf6d0686c38ccb0c6e229f
Nicolas Dupont <npg.dupont@gmail.com>
2019-05-16 14:36:20 +0200

    Add script to deploy

---
+++ internals/scripts/deploy/initiate.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+### Configuration ###
+
+SERVER=cloud@disinfo.quaidorsay.fr
+APP_DIR=/home/cloud/political-ads-legality
+
+
+### Library ###
+
+function run()
+{
+  echo "Running: $@"
+  "$@"
+}
+
+
+### Automation steps ###
+
+run scp -rp $PWD/build $SERVER:$APP_DIR
